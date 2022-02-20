097e0d6f17fc40ca2243ce83d851aae8c8696ffd
ROY <ROY@DESKTOP-26H23PD>
2022-02-20 18:02:52 +0530

    Add project files.

---
+++ primeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Prime
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n, i, j;
+            n = int.Parse(Console.ReadLine());
+            for (i = 2; i <= n; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    int m = i % j;
+
+                    Console.WriteLine(m);
+
+
+                }
+
+            }
+        }
+    }
+}
+}
